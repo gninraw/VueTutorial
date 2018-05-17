ce1ef8ca655f3ea8ff620af0e4c322d9825ea035
Y.S.LIM <gninraw@lonk.co.kr>
2018-05-17 10:59:11 +0900

    2차 커밋

---
+++ Code/component/greeting.vue
@@ -0,0 +1,22 @@
+<template>
+    <p> {{ greeting }} World </p>
+</template>
+
+<script>
+    module.export = {
+        data : function() {
+            return {
+                greeting : 'Hello'
+            }
+        }
+    }
+
+</script>
+
+<style>
+p {
+    font-size : 2em;
+    text-align : center;
+}
+
+</style>
